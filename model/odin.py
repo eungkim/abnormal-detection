4b9db21aecf76a85a785f7cb79a1855934527668
eung <keb7952@gmail.com>
2020-12-30 01:06:28 +0900

    concatenated class named ODIN is constructed

--- model/odin.py
+++ model/odin.py
@@ -3,46 +3,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+#Temperature Scaling part
 class TempScaling(nn.Module):
-    def __init__(self, temp):
+    def __init__(self, model, temp):
         super(TempScaling, self).__init__()
+        self.model = model
         self.temp = temp
 
-        #(batch, num_classes)
-    def forward(self, x):
-        #(batch, num_classe)
+    def forward(self, data):
+        #x = [batch, num_classes]
+        x = self.model(data)
+        #numer = [batch, num_classe]
         numer = torch.exp(x/self.temp)
-        #(batch, 1)
+        #denom = [batch, 1]
         denom = torch.sum(numer, dim=1).view(-1, 1)
-        #(batch, num_classes)
+        #out = [batch, num_classes]
         out = torch.div(numer, denom)
         return out
 
 
-def input_preprocess(model, x, eps): #input_preprocess(model=YourModel(), x=yourdata, eps=eps)
-    x.requires_grad = True
-    #(batch, num_class)
-    out = model(x)
-    #(batch)
-    out, _ = torch.max(out, dim=1)
-    out = torch.sum(out)
-    out.backward(retain_graph=True)
-    grad = -x.grad
-
-    perturbation = torch.multiply(torch.sign(grad), eps)
-    x_tilde = x - perturbation
-    return x_tilde
-
-
-class OutDetector(nn.Module):
-    def __init__(self, eps):
-        super(OutDetector, self).__init__()
+#Input Preprocessing part
+class InputPreprocess(nn.Module):
+    def __init__(self, model, eps):
+        super(InputPreprocess, self).__init__()
         self.eps = eps
+        self.model = model
 
-        #(batch, num_class)
     def forward(self, x):
-        #(batch)
+        x.requires_grad = True
+        #out = [batch, num_class]
+        out = self.model(x)
+        #out = [batch]
+        out, _ = torch.max(out, dim=1)
+        out = torch.sum(out)
+        out.backward(retain_graph=True)
+        grad = -x.grad
+
+        perturbation = torch.multiply(torch.sign(grad), eps)
+        x_tilde = x - perturbation
+        return x_tilde 
+
+
+#Out=of-distribution Detector part
+class OutDetector(nn.Module):
+    def __init__(self, sigma):
+        super(OutDetector, self).__init__()
+        self.sigma = sigma
+
+        #x = [batch, num_class]
+    def forward(self, x):
+        #max_softmax = [batch]
         max_softmax = torch.max(x, dim=1)
-        #(batch)
-        out = max_softmax > self.eps 
-        return out #(batch), Boolean Tensor, True in-dist, False out-dist 
+        #out = [batch]
+        out = max_softmax > self.sigma 
+        return out #Boolean Tensor, True out-dist, False in-dist 
+
+
+class ODIN(nn.Module):
+    """
+    temp_scaler = TempScaling(base_model, temp)
+    input_preprocss = InputPreprocess(TempScaling(base_model, temp), eps)
+    ood_detector = OutDetector(sigma)
+    """
+    def __init__(self, temp_scaler, input_preprocess, ood_detector):
+        super(ODIN, self).__init__()
+
+        self.temp_scaler = temp_scaler
+        self.input_preprocess = input_preprocess 
+        self.ood_detector = ood_detector
+
+    def forward(self, x):
+        x_tilda = self.input_preprocess(x) 
+        out = self.temp_scaler(x_tilda)
+        out = self.ood_detector(out)
+        return out
+
